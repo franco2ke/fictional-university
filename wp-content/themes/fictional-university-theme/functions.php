be115f62c760dc5f88f50dcb13ccf082d27a1091
Francis Wafula <francis@paon.co.ke>
2024-03-27 09:38:07 +0300

    ft-user-login: improve user login experience - redirect non-admin users (subscribers) to homepage on login - hide top admin bar for non-admin users

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -144,3 +144,27 @@ function universityMapKey($api)
   $api['key'] = 'AIzaSyBozRvDjhNyHQDJWe8m6xf2Po7E59vbZUI';
   return $api;
 }
+
+// NOTE: Redirect subscriber accounts out of admin page to homepage
+add_action('admin_init', 'redirectSubsToFrontend');
+
+function redirectSubsToFrontend()
+{
+  $ourCurrentUser = wp_get_current_user();
+  // Only redirect user to frontend if user only has subscriber role
+  if (count($ourCurrentUser->roles) == 1 && $ourCurrentUser->roles[0] == 'subscriber') {
+    wp_redirect(site_url('/'));
+    exit;
+  }
+}
+
+// NOTE: Hide top admin bar for subscriber roles
+add_action('wp_loaded', 'noSubsAdminBar');
+
+function noSubsAdminBar()
+{
+  $ourCurrentUserRoles = wp_get_current_user()->roles;
+  if (count($ourCurrentUserRoles) == 1 && $ourCurrentUserRoles[0] == 'subscriber') {
+    show_admin_bar(false);
+  }
+}
