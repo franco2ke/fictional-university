ba19d1e444ddd0e2cca644b4005786ee6efa0d94
Francis Wafula <francis@paon.co.ke>
2024-03-28 12:53:58 +0300

    ft-notes-page: add edit note functionality - add listener for edit note events (edit button) - add animation to hide successfully deleted notes - enable individual note selection via ID - add data-id attribute to each note; on the <li> parent element - add 'readonly' attribute to inputs to make them readonly by default - add 'save' notes button and make it visible on edit - add input border animation to highlight input state change

--- wp-content/themes/fictional-university-theme/src/modules/MyNotes.js
+++ wp-content/themes/fictional-university-theme/src/modules/MyNotes.js
@@ -6,20 +6,25 @@ class MyNotes {
   }
 
   events() {
-    $(".delete-note").on("click", this.deleteNote);
+    $(".delete-note").on("click", this.deleteNote); // delete the note
+    $(".edit-note").on("click", this.editNote); // edit the note
   }
 
   // Methods
-  deleteNote() {
+  deleteNote(e) {
+    // get the list element to obtain the note ID from its data attribute
+    let thisNote = $(e.target).parents("li");
     // ajax method used to send POST/DELETE requests
     $.ajax({
       beforeSend: (xhr) => {
         xhr.setRequestHeader("X-WP-Nonce", universityData.nonce);
       },
-      url: universityData.root_url + "/wp-json/wp/v2/note/130",
+      url:
+        universityData.root_url + `/wp-json/wp/v2/note/${thisNote.data("id")}`,
       type: "DELETE",
       success: (response) => {
-        console.log("Congrats");
+        thisNote.slideUp();
+        console.log("Congratso");
         console.log(response);
       },
       error: (response) => {
@@ -27,6 +32,19 @@ class MyNotes {
         console.log(response);
       },
     });
+  }
+
+  // Note, edit the note
+  editNote(e) {
+    // get the list element to obtain the note ID from its data attribute
+    let thisNote = $(e.target).parents("li");
+    // make input elements editable
+    thisNote
+      .find(".note-title-field, .note-body-field")
+      .removeAttr("readonly")
+      .addClass("note-active-field");
+    // make the button visible
+    thisNote.find(".update-note").addClass("update-note--visible");
   }
 }
 
