b2ba5554e2ff664ae037529d8b8b609101d87070
Francis Wafula <francis@paon.co.ke>
2024-04-09 14:27:50 +0300

    ft-note-security: 1. Strip 'Private' auto generated word from note title 2. Add filter to strip all code; html and js from subscriber/admin submitted content. Content sanitization. 3. Edit roles for maximum security

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -199,11 +199,16 @@ function ourLoginTitle()
 }
 
 // Force new note posts to be private
-// In WP 'filter' can also mean 'modify'
+// In WP 'filter' can also mean 'modify', intercept post data before its saved into DB
 add_filter('wp_insert_post_data', 'makeNotePrivate');
 
 function makeNotePrivate($data)
 {
+  if ($data['post_type'] == 'note') {
+    // sanitize the content the user submits
+    $data['post_content'] = sanitize_textarea_field($data['post_content']);
+    $data['post_title'] = sanitize_text_field($data['post_title']);
+  }
   // only filter the note post type
   // do nothing if post has been trashed, deleted
   if ($data['post_type'] == 'note' && $data['post_status'] != 'trash') {
