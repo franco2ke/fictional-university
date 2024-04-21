c143cd9c886847adf857fbc8ad41fd12275d543b
Francis Wafula <francis@paon.co.ke>
2024-04-21 12:19:20 +0300

    ft-plugins: create plugin #1

---
+++ wp-content/plugins/our-first-unique-plugin/our-first-unique-plugin.php
@@ -0,0 +1,32 @@
+<?php
+
+/* 
+* Plugin Name: Our Test Plugin
+* Description: A truly amazing plugin.
+* Version: 1.0
+* Author: Francis
+* Author URI: https://paon.co.ke
+*/
+
+class WordCountAndTimePlugin
+{
+  function __construct()
+  {
+    add_action('admin_menu', array($this, 'adminPage'));
+  }
+
+  function adminPage()
+  {
+    add_options_page('Word Count Settings', 'Word Count', 'manage_options', 'word-count-settings-page', array($this, 'ourHTML'));
+  }
+
+  function ourHTML()
+  { ?>
+    <div class="wrap">
+      <h1>Word Count Settings</h1>
+    </div>
+
+<?php }
+}
+
+$wordCountAndTimePlugin = new WordCountAndTimePlugin();
