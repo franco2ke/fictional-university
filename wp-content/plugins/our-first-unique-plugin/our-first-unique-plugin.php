007a5553d0792d859d55f06bea7e30ed0803e20c
Francis Wafula <francis@paon.co.ke>
2024-04-22 12:43:52 +0300

    ft-plugins: saving & retrieving plugin settings

--- wp-content/plugins/our-first-unique-plugin/our-first-unique-plugin.php
+++ wp-content/plugins/our-first-unique-plugin/our-first-unique-plugin.php
@@ -14,34 +14,83 @@ class WordCountAndTimePlugin
   function __construct()
   {
     // Add action hooks to execute the 'adminPage' and 'settings' functions
-    add_action('admin_menu', array($this, 'adminPage')); // Add admin menu page
-    add_action('admin_init', array($this, 'settings')); // Initialize settings
+    add_action('admin_menu', array($this, 'adminPage')); // construct the admin options/settings page
+    add_action('admin_init', array($this, 'settings')); // Initialize settings (create in db, frontend and link them)
   }
 
   // Function to define plugin settings
   function settings()
   {
-    // Add a settings section to the plugin settings page ( with no subheadings, content)
+    // Create a settings section that will be added to the plugin settings page ( with no subheadings, content)
     add_settings_section('wcp_first_section', null, null, 'word-count-settings-page');
 
+    // NOTE: Field 1: location 
     // Add a settings field to the 'wcp_first_section' for the display location option
     // A plugin can have multiple settings pages hence why page slug required
     add_settings_field('wcp_location', 'Display Location', array($this, 'locationHTML'), 'word-count-settings-page', 'wcp_first_section');
-
-    // Register a setting named 'wcp_location' with the wordpress settings API
+    // Register our setting named 'wcp_location' with the wordpress settings API
+    // so that $_POST & security handling is done for us and our callback function just has to echo the <input>
     register_setting('wordcountplugin', 'wcp_location', array(
-      'sanitize_callback' => 'sanitize_text_field', // Sanitize callback function to ensure data security
-      'default' => 0 // Default value for the setting
+      'sanitize_callback' => array($this, 'sanitizeLocation'), // Sanitize callback function to ensure data security
+      'default' => '0' // Default value for the setting
     ));
+
+    // NOTE: Field 2: Headline Text
+    // Add a settings field to the 'wcp_first_section' for the display location option
+    // A plugin can have multiple settings pages hence why page slug required
+    add_settings_field('wcp_headline', 'Headline Text', array($this, 'headlineHTML'), 'word-count-settings-page', 'wcp_first_section');
+    // Register our setting named 'wcp_location' with the wordpress settings API
+    // so that $_POST handling is done for us and our callback function just has to echo the <input>
+    register_setting('wordcountplugin', 'wcp_headline', array(
+      'sanitize_callback' => 'sanitize_text_field', // Sanitize callback function to ensure data security
+      'default' => 'Post Statistics' // Default value for the setting
+    ));
+
+    // NOTE: Field 3: Wordcount
+    add_settings_field('wcp_wordcount', 'Word Count', array($this, 'checkboxHTML'), 'word-count-settings-page', 'wcp_first_section', array('theName' => 'wcp_wordcount')); // Front end setup
+    register_setting('wordcountplugin', 'wcp_wordcount', array('sanitize_callback' => 'sanitize_text_field', 'default' => '1')); // Back end setup
+
+    // NOTE: Field 4: Charactercount
+    add_settings_field('wcp_charactercount', 'Character Count', array($this, 'checkboxHTML'), 'word-count-settings-page', 'wcp_first_section', array('theName' => 'wcp_charactercount'));
+    register_setting('wordcountplugin', 'wcp_charactercount', array('sanitize_callback' => 'sanitize_text_field', 'default' => '1'));
+
+    // NOTE: Field 5: Readtime
+    add_settings_field('wcp_readtime', 'Read Time', array($this, 'checkboxHTML'), 'word-count-settings-page', 'wcp_first_section', array('theName' => 'wcp_readtime')); // create front end
+    register_setting('wordcountplugin', 'wcp_readtime', array('sanitize_callback' => 'sanitize_text_field', 'default' => '1'));
   }
+
+  // custom validation
+  function sanitizeLocation($input)
+  {
+    // check if value of Location is valid ( 0 or 1)
+    if ($input != '0' and $input != '1') {
+      add_settings_error('wcp_location', 'wcp_location_error', 'Display Location must be either beginning or end');
+
+      return get_option('wcp_location');
+    }
+    return $input;
+  }
+
+  // reusable checkbox function (for enabling character count, word count, read time)
+  function checkboxHTML($args)
+  { ?>
+    <input type="checkbox" name="<?php echo $args['theName']; ?>" value="1" <?php checked(get_option($args['theName']), "1"); ?>>
+  <?php }
+
+  // Function to render the markup for the headline text field
+  function headlineHTML()
+  { ?>
+    <input type="text" name="wcp_headline" value="<?php echo esc_attr(get_option('wcp_headline')); ?>">
+
+  <?php }
 
   // Function to render the HTML for the location setting field
   function locationHTML()
   { ?>
     <!-- HTML markup for the select dropdown -->
     <select name="wcp_location">
-      <option value="0">Beginning of post</option>
-      <option value="1">End of post</option>
+      <option value="0" <?php selected(get_option('wcp_location'), '0'); ?>>Beginning of post</option>
+      <option value="1" <?php selected(get_option('wcp_location'), '1'); ?>>End of post</option>
     </select>
   <?php }
 
@@ -61,10 +110,10 @@ class WordCountAndTimePlugin
       <!-- Form for plugin settings -->
       <form action="options.php" method="POST">
         <?php
-        // Output security fields (nonce)
+        // display hidden fields and handle security of your options form (nonce) + name option group
         settings_fields('wordcountplugin');
 
-        // Output settings sections and fields
+        // Output settings sections and fields registered with the WordPress settings API
         do_settings_sections('word-count-settings-page');
 
         // Output submit button
